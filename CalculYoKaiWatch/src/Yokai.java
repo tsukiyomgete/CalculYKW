6cd658cd6d67ba31079197e64ae0242a424602cf
Naim Talsi <naim.talsi@universite-paris-saclay.fr>
2026-02-18 13:05:48 +0100

    v0.2.1 ajout du constructeur Yokai

--- CalculYoKaiWatch/src/Yokai.java
+++ CalculYoKaiWatch/src/Yokai.java
@@ -1,6 +1,6 @@
 public class Yokai {
     String nom;
-    int rangYokai;
+    Rank rangYokai;
     Attitude attitudeYoKai;
     face_icon faceYoKai;
     boolean canEvolve;
@@ -11,11 +11,29 @@ public class Yokai {
     int BaseSPR;
     int BaseDEF;
     int BaseSPE;
-    Elemental elementType;
     Elemental weakness;
-    Elemental resist;
+    Elemental resistance;
 
     Item[] HoldingItem;
+
+    Yokai(String nom, Rank rangYokai, Attitude attitudeYoKai, face_icon faceYoKai, boolean canEvolve, boolean  canFusion, FoodClass favFood, int BaseSTR, int BaseSPR, int BaseDEF, int BaseSPE, Elemental weak, Elemental rest, int maxItem)
+    {
+        this.nom = nom;
+        this.rangYokai = rangYokai;
+        this.attitudeYoKai = attitudeYoKai;
+        this.faceYoKai = faceYoKai;
+        this.canEvolve = canFusion;
+        
+        this.favFoodType = favFoodType;
+        
+        this.BaseSTR = BaseSTR;
+        this.BaseSPR = BaseSPR;
+        this.BaseDEF = BaseDEF;
+        this.BaseSPE = BaseSPE;
+        this.weakness = weak;
+        this.resistance = rest;
+        this.HoldingItem = new Item[maxItem];
+    }
 
 
 
