4dc9990ad69eb639fbdf5066375af5f9bde76cc8
Naim <web44653@gmail.com>
2026-02-19 11:00:00 +0100

    Update Attitude.java

--- CalculYoKaiWatch/src/Attitude.java
+++ CalculYoKaiWatch/src/Attitude.java
@@ -6,20 +6,7 @@ public class Attitude {
     int def;
     int spe;
 
-    Attitude[] attitude = {
-    		new Attitude("gentle",26,0,13,0,0),
-    		new Attitude("tender",52,0,0,0,0),
-    		new Attitude("grouchy",26,13,0,0,0),
-    		new Attitude("rough",0,26,0,0,0),
-    		new Attitude("logical",0,0,13,0,13),
-    		new Attitude("brainy",0,26,0,0,0),
-    		new Attitude("careful",0,0,13,13,0),
-    		new Attitude("calm",0,0,0,26,0),
-    		new Attitude("twisted",0,0,0,0,13),
-    		new Attitude("cruel",0,0,0,0,26),
-    		new Attitude("helpful",26,0,0,0,13),
-    		new Attitude("devoted",0,13,0,13,0)
-		};
+    
     
 
     Attitude(String nom, int Health, int Strength, int Spirit, int Defense, int Speed)
